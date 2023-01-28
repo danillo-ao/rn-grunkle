2c7b90c3b1b62b4be2d6b1b8f563a86e8b2c04d4
Danillo Oliveira <danillo.alves.o@gmail.com>
2023-01-28 16:14:54 -0300

    chore: template structure

---
+++ template.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // Placeholder used to rename and replace in files
+  // package.json, index.json, android/, ios/
+  placeholderName: 'template_grunkle',
+
+  // Placeholder title that will be replaced in values.xml and Info.plist with title provided by the user.
+  // We default this value to 'Hello App Display Name', which is default placeholder in react-native template.
+  titlePlaceholder: 'template_grunkle',
+
+  // Directory with template
+  templateDir: './template',
+};
